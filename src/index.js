72cdd55dbb97f65b0710fd30696d7c36dd992bb9
GabbyHolics <karenthgabriela@gmail.com>
2021-05-12 20:44:04 -0400

    Modifique el JS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import cipher from './cipher.js';
 
+import cipher from './cipher.js';
+console.log("Hola")
+function Descifrar() {
+    
+ var recibir = document.getElementById("phrases").value; 
+}
+
+window.Descifrar= Descifrar ;
+// 
 console.log(cipher);
